b6ef7d50bbd2d4e94f3618b3b200be64b9d5ad68
abdulrehman990 <mabdulf64@gmail.com>
2026-03-07 22:44:04 +0500

    This is my first change

---
+++ First_day.py
@@ -0,0 +1,100 @@
+
+#Practice: Personal Information Program
+name = "Abdul rehman"
+age =  23
+degree = "BS Software Engineering"
+university = "Riphah International University Faisalabad"
+
+print("My name is ", name)
+print("I am", age , "year old" )
+print("I am studing in",degree)
+print("My university in which i am studying", university)
+
+print(type(name))
+print(type(age))
+print(type(degree))
+print(type(university))
+
+
+#Practice: Simple Calculator
+num1 =  5
+num2 = 10
+
+print(num1+num2)
+print(num1*num2)
+print(num1-num2)
+print(num1/num2)
+print(num1**num2)
+print(num1%num2)
+
+
+#Practice: Boolean Program
+marks = 60
+
+print("Marks:", marks)
+print("Passed:", marks > 50)
+
+#Practice: String Example
+
+name = 'Abdul rehman'
+first_name = "Abdul"
+last_name = """Rehman"""
+
+print(name)
+print(first_name)
+print(last_name)
+
+print(name) ; print(first_name) ; print(last_name)
+
+single = 'Python is easy'
+double = "Programming is fun"
+triple = """Python allows
+multiple lines
+using triple quotes"""
+
+print(single)
+print(double)
+print(triple)
+
+#Practice: Type Conversion
+num3 = int (3.5)
+num4 = 5
+
+print(type(num3))
+
+
+num3 = str (3.5)
+num4 = "Abdul rehman"
+
+print(num4 + num3)
+
+n1 = "Abdul"
+n2 = "Rehman"
+
+print(n1 , end=" ")
+print(n2)
+
+# new one
+""" shoes_price = 3000
+bag_price = 2000
+shirt_price = 4000
+
+shoes = input("Enter the shirt name")
+bag = input("Enter tha bag brand name")
+shirt = input("Enter the shirt brand")
+
+print("I want to buy this brand shirt" , shirt ,"The price of this shirt is " , shirt_price)
+"""
+
+if 5 > 2:
+ print("Five is greater than two!")
+
+
+nn1 = " hELLO"
+nn2 = "World"
+
+print(nn1 + nn2)
+
+print(nn1[1: 3])
+
+print(nn1[-3: -1])
---
+++ second_day.py
@@ -0,0 +1,59 @@
+""" 
+# Program no 1
+first_name = input("Enter the first name = ")
+
+print(first_name)
+print(len(first_name))
+
+# Program no 2
+first = "I bought a car in $  in usa of $22191"
+print(first.count("$"))
+
+mark = int( input("Enter the marks"))
+if(mark >= 80):
+    grade = "A"
+elif(mark >= 70 and mark < 80):
+        grade = "B"
+
+elif(mark >= 60 and mark < 70):
+        grade = "C"
+
+elif(mark >= 50 and mark < 60 ):
+       grade = "D"
+
+else:
+      grade = "F"
+
+print("Grade of the students", grade)
+
+
+# Program no 3
+num= int( input("Enter the Number"))
+
+if(num % 2 == 0):
+    print("Number is even")
+
+else:
+    print("Number is odd")
+
+# Program no 4
+num1 = int( input("Enter the Number"))
+num2 = int( input("Enter the Number"))
+num3 = int( input("Enter the Number"))
+
+if (num1 > num2 and num3):
+    print("Num1 is greater then num2 and num3")
+elif (num1 >= num2 and num3):
+    print("Num1 is equal to num2 and num3")
+else:
+    print("Num1 is smaller than num2 and num3")
+    """
+
+# Program no 5
+num= int( input("Enter the Number"))
+
+if(num % 7 == 0):
+    print("Multiple of 7")
+
+else:
+    print("Not a Multiple of 7")
