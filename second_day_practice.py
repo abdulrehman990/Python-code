811f69bc8c60f3b0a4ce3313dd26f0bf6864333f
abdulrehman990 <mabdulf64@gmail.com>
2026-03-07 22:58:14 +0500

    New code added

--- second_day_practice.py
+++ second_day_practice.py
@@ -12,3 +12,7 @@ print("The lenght of the name is :", len(Name))
 # Question 2 : Take a name from the user and convert the first letter into capital. """
 Name =  "abdul rehman"
 print( Name.capitalize ())
+
+# Question 3: Take a sentence from the user and count how many times "a" appears.
+sub = "Pakistan is amazing"
+print(sub.count("a"))
