ff410aa9ea0a4c3f34fecc9b1ec5bab1cfa64ecd
abdulrehman990 <mabdulf64@gmail.com>
2026-03-07 22:56:07 +0500

    Chnages made

--- second_day_practice.py
+++ second_day_practice.py
@@ -1,14 +1,14 @@
 """ Question 1
-
 Take a string from the user and print:
-
 Length of the string
-
 First character
-
 Last character """
-
+"""
 Name =  input("Enter the name: ")
 
 print("My name is :" , Name)
 print("The lenght of the name is :", len(Name))
+
+# Question 2 : Take a name from the user and convert the first letter into capital. """
+Name =  "abdul rehman"
+print( Name.capitalize ())
