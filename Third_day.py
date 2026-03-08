bb552a056411d04c391a00b1cd2866b6aea3a2ae
abdulrehman990 <mabdulf64@gmail.com>
2026-03-08 16:48:50 +0500

    Done

--- Third_day.py
+++ Third_day.py
@@ -1,5 +1,5 @@
 # Program no 1
-movies = input("Enter your favourite movie name: ")
+""" movies = input("Enter your favourite movie name: ")
 movies1 = input("Enter your favourite movie name: ")
 movies2 = input("Enter your favourite movie name: ")
 print(movies)
@@ -11,4 +11,15 @@ movie_list.append(movies)
 movie_list.append(movies1)
 movie_list.append(movies2)
 
-print(movie_list)
+print(movie_list) """
+
+#Program no 2
+
+palidorm = [1,3,3,1]
+palidorm1 = palidorm.copy()
+palidorm1.reverse()
+print(palidorm)
+if(palidorm1 == palidorm):
+    print("You are right")
+else:
+    print("Not")
