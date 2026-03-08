13c59b003d2292b7e5171f63f58de46d1c2a3ec8
abdulrehman990 <mabdulf64@gmail.com>
2026-03-08 16:54:31 +0500

    changes made

--- Third_day.py
+++ Third_day.py
@@ -15,11 +15,21 @@ print(movie_list) """
 
 #Program no 2
 
-palidorm = [1,3,3,1]
+""" palidorm = [1,3,3,1]
 palidorm1 = palidorm.copy()
 palidorm1.reverse()
 print(palidorm)
 if(palidorm1 == palidorm):
     print("You are right")
 else:
-    print("Not")
+    print("Not")"""
+
+#Program no 3
+tuple = ("A","C","A","B","A","F","A","E","D")
+
+tuple2= tuple.count("A")
+print(tuple2)
+
+list = ["A","C","A","B","A","F","A","E","D"]
+print(list.sort())
+print(list)
