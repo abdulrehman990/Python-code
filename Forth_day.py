fb9a0f9a8da0ed54dc9b1dd4f874524ab46d2879
abdulrehman990 <mabdulf64@gmail.com>
2026-03-11 01:41:42 +0500

    New added

--- Forth_day.py
+++ Forth_day.py
@@ -1,4 +1,5 @@
-info = {
+# Program no 1
+""" info = {
     "Title" : "The Moon Rise",
     "author" : "Mickeal Jackson",
     "Price" : "1200",
@@ -7,3 +8,13 @@ info = {
 
 print("Author :" , info["author"])
 print("Price :", info["Price"])
+"""
+#Program no 2
+car = {
+    "brand" : "Hyndai",
+    "model" : "SUV" , 
+    "Year" :   "2022"
+}
+print("Before Update : ", car.keys())
+car.update({"color" : " White"})
+print("After Update : ", car.keys())
