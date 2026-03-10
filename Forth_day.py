bc5c0b24610374533ca082466532d0fb5714219a
abdulrehman990 <mabdulf64@gmail.com>
2026-03-11 01:45:06 +0500

    DOne

--- Forth_day.py
+++ Forth_day.py
@@ -10,6 +10,7 @@ print("Author :" , info["author"])
 print("Price :", info["Price"])
 """
 #Program no 2
+"""
 car = {
     "brand" : "Hyndai",
     "model" : "SUV" , 
@@ -18,3 +19,16 @@ car = {
 print("Before Update : ", car.keys())
 car.update({"color" : " White"})
 print("After Update : ", car.keys())
+
+"""
+
+#Program no 3
+student = {
+    "name": "Ahmed",
+    "age": 21,
+    "course": "Python"
+}
+
+print("It will print all of the keys in the students data" , " ",  student.keys())
+print("It will print all of the Values in the students data" ,  " ", student.values())
+print("It will print all of the items in the students data" , " ",   student.items())
