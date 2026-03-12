2f0be24263a58dd39f6e9a5dddb96cb8e5af8459
abdulrehman990 <mabdulf64@gmail.com>
2026-03-12 17:54:08 +0500

    Program done

--- Fifth_day.py
+++ Fifth_day.py
@@ -9,11 +9,28 @@ student = {
 print(  
 student.values()
 )
-"""
-
+#program no 2
 students = {
     "Abdul rehman" : 90,
     "Al Rayyan" : 92,
     "Taha raza" :91
 }
 print("Marks of the students are: " , students.values())
+
+#program no 4
+data = {
+    "name" : "Abdul rehman",
+    "age" : 29,
+    "course" : "BSSE",
+  }
+
+user = input("Enter the value : " )
+value = data.get(user)
+
+
+if user is None:
+    print("Error")
+else:
+    print("value : ", value)
+
+"""
