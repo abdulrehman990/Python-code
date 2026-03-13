abcc289158cbf01318fbb45d35c1e9203d93cb9b
abdulrehman990 <mabdulf64@gmail.com>
2026-03-13 23:35:12 +0500

    code added

--- Sixth_day.py
+++ Sixth_day.py
@@ -11,10 +11,16 @@ while i >=1:
     print("Counting" , i)
     i -= 1
 
-"""
 #Program no 3
 number = int(input("Enter the number : " ))
 count = 1
 while count <= 10 :
     print("The table of given no is :" , number ,"*" , count ,"=" , number * count)
     count += 1
+"""
+#Program no 4
+item = [1,4,9,16,25,36,49,64,81,100]
+index = 0
+while index < len(item) :
+    print("list printed : ",item[index])
+    index += 1
