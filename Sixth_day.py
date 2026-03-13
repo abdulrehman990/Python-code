cf4152e0bbb9b441986e77c928a519ac8e8cc100
abdulrehman990 <mabdulf64@gmail.com>
2026-03-13 23:22:21 +0500

    code added

--- Sixth_day.py
+++ Sixth_day.py
@@ -5,9 +5,16 @@ while i <=100:
     print("Counting" , i)
     i += 1
 
-"""
 #Program no 2
 i= 100
 while i >=1:
     print("Counting" , i)
     i -= 1
+
+"""
+#Program no 3
+number = input("Enter the number : " )
+count = 1
+while number <= count:
+    print("The table of given no is :" , number ," *" , count ,"=" , number*count)
+    count += 1
