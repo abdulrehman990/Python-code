a8d1f9465da852af5b6d4182e1b40193fc054d52
abdulrehman990 <mabdulf64@gmail.com>
2026-03-15 18:20:08 +0500

    code added

--- 20_PROGRAM_FOR_GRIP_ON_PYTHON.py
+++ 20_PROGRAM_FOR_GRIP_ON_PYTHON.py
@@ -7,7 +7,22 @@ while i <= 100:
         print(i)
     i += 1
 """
+"""
 #Take a number from the user and count how many digits it has.
 data = input("Enter the word : ")
 print("The given word is : ",data)
 print("The character in given word is : ",len(data))
+"""
+student = ["name","digit" ,"word"]
+
+index = 0 
+count= 0
+data = input("Enter the word : ")
+while index < len(student):
+    if data == student[index]:
+        print("DATA IS IN IT ")
+
+    index += 1
+
+count +=1
+print("The count is :",count)
