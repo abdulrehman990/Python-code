8be1919ddd7f5b7b6d7c94d1baec30c645d99057
abdulrehman990 <mabdulf64@gmail.com>
2026-03-15 18:16:46 +0500

    code added

--- 20_PROGRAM_FOR_GRIP_ON_PYTHON.py
+++ 20_PROGRAM_FOR_GRIP_ON_PYTHON.py
@@ -1,7 +1,13 @@
 #Print numbers 1 to 100, but print only numbers divisible by 3.
+"""
 i= 1
 while i <= 100:
 
     if i % 3 == 0:  
         print(i)
     i += 1
+"""
+#Take a number from the user and count how many digits it has.
+data = input("Enter the word : ")
+print("The given word is : ",data)
+print("The character in given word is : ",len(data))
