19e12d6fb746466d9ad23f5146ae1637bc1d45dc
abdulrehman990 <mabdulf64@gmail.com>
2026-03-15 19:16:50 +0500

    code added

--- 20_PROGRAM_FOR_GRIP_ON_PYTHON.py
+++ 20_PROGRAM_FOR_GRIP_ON_PYTHON.py
@@ -13,8 +13,17 @@ data = input("Enter the word : ")
 print("The given word is : ",data)
 print("The character in given word is : ",len(data))
 """
-
+"""
 #Take a word from the user and print it in reverse.
 name = input("Enter the word : ")
 reversed_name = name[ :: -1 ]
 print("The reverse list is : ",reversed_name)
+"""
+#Print only numbers greater than 10.
+numbers = [4, 7, 12, 3, 9, 20]
+index = 0
+
+while index < len(numbers):
+    if numbers[index] > 10:
+        print(numbers[index])
+    index += 1  
