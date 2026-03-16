af6989eb852b7ce8fde7d0b27c75a437e487d9ad
abdulrehman990 <mabdulf64@gmail.com>
2026-03-17 00:16:02 +0500

    code added

--- For_loop.py
+++ For_loop.py
@@ -4,12 +4,12 @@ while i <= 10:
     print(i)
     i +=1
 """
-name = 1
+rows = 6
 
-for i in name :
-    if(name == 5):
-        break
-    print(i,i+1,)
-    name += 1
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(j, end= " ")
+    
+    print('')
 
 
