6777638a9529ad219e861e8a0bf594f8ccf2f45f
abdulrehman990 <mabdulf64@gmail.com>
2026-03-17 16:29:47 +0500

    code added

--- For_loop.py
+++ For_loop.py
@@ -15,7 +15,6 @@ for i in range(1,rows+1):
 
 
 
-"""
 
 i = 1
 new = int(input("Enter the digit : "))
@@ -24,3 +23,11 @@ while i <= new :
     sum = sum + i
     i += 1
 print("Sum is : ", sum)
+"""
+
+rows = 6
+
+for i in range (1,rows + 1):
+    for j in range (1,i+1):
+        print(j, end=" ")
+    print(' ')
