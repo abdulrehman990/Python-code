89fa9a792e3d0d4f9988853fe82a9c21a13a4c1e
abdulrehman990 <mabdulf64@gmail.com>
2026-03-17 23:47:39 +0500

    code added

--- For_loop.py
+++ For_loop.py
@@ -31,10 +31,17 @@ for i in range (1,rows + 1):
         print(j, end=" ")
     print(' ')
 
-"""
 
 data = int(input("Enter the value : "))
 i = 1
 while data >= i:
     print("The table of given number is ",data , "*",i,"=",data*i )
     i += 1
+"""
+numbers = [12, 75, 150, 180, 145, 525, 50]
+index = 0
+max = numbers[0]
+while index < len(numbers):
+    if(max % 5 ==0):
+        print(max)
+    index += 1
