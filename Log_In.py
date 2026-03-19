c53d77de164de1d5d509d9b086be47a72a6519c1
abdulrehman990 <mabdulf64@gmail.com>
2026-03-20 00:29:08 +0500

    code added

--- Log_In.py
+++ Log_In.py
@@ -19,3 +19,6 @@ while True:
      else:
         print("Invalid Password! Try again.")
         print("Your password must have at least 7 characters and Special characters")
+name = input("Enter the Name : ")
+degree = input("Enter the Degree : ")
+roll_no = input("Enter the Roll no : ")
