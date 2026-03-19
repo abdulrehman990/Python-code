6eb08c3d04a78031343fc184ca4947847aac2f10
abdulrehman990 <mabdulf64@gmail.com>
2026-03-20 00:55:40 +0500

    code added

--- Log_In.py
+++ Log_In.py
@@ -29,7 +29,7 @@ marks = { "Math" : input("Enter you Math marks : " ),
          "English" : input("Enter you English marks : " ),
          "Physics" : input("Enter you Math marks : " ),}
 total_marks = marks["English"] + marks["Math"] + marks["Physics"]
-percentage = (int(total_marks) / 100  * 100)
+percentage = int((total_marks)) / 100  * 100
 
 if(percentage >= 80):
     "Grade A"
