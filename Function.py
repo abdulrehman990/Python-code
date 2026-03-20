c678df8c6b57bc72248453344df84916eb85d827
abdulrehman990 <mabdulf64@gmail.com>
2026-03-20 16:10:26 +0500

    code added

---
+++ Function.py
@@ -0,0 +1,34 @@
+"""
+#Program no 1
+cities = ["faisalabad","lahore","islamabad","rawalpindi","karachi"]
+
+def print_len(lists):
+    print(len(lists))
+
+print_len(cities)
+
+#program no 2
+cities = {"faisalabad","lahore","islamabad","rawalpindi","karachi"}
+
+def print_list(lists):
+    print(lists)
+
+print_list(cities)
+
+#program no 3
+cities = {"faisalabad","lahore","islamabad","rawalpindi","karachi"}
+
+def print_list(lists):
+    for list in cities:
+        print(list, end=" ")
+
+print_list(cities)
+"""
+
+#program no 4
+def factorial(n):
+    for i in range(1,n+1):
+        n *= i
+        print(n)
+
+factorial(7)
